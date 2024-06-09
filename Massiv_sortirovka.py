d850427ed17ebb3d2a355023d97bdcb637821b0a
jimaleIka <157210109+jimaleIka@users.noreply.github.com>
2024-06-09 15:18:26 +0700

    Add files via upload

---
+++ Massiv_sortirovka.py
@@ -0,0 +1,76 @@
+#Сортировка пузырьком:
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+#Сортировка вставками:
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+#Сортировка слиянием:
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+#Быстрая сортировка
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+#Сортировка подсчётом
+
+def counting_sort(arr):
+    max_num = max(arr)
+    min_num = min(arr)
+    range_of_elements = max_num - min_num + 1
+    count_arr = [0 for _ in range(range_of_elements)]
+    output_arr = [0 for _ in range(len(arr))]
+
+    for num in arr:
+        count_arr[num - min_num] += 1
+
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i - 1]
+
+    for i in range(len(arr) - 1, -1, -1):
+        output_arr[count_arr[arr[i] - min_num] - 1] = arr[i]
+        count_arr[arr[i] - min_num] -= 1
+
+    return output_arr
